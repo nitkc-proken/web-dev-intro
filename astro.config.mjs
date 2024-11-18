0bdc548203af0733d6d2344b0d38457e7295758f
naotiki <contact@naotiki.me>
2024-11-18 21:38:56 +0900

    add biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,34 @@
+import starlight from "@astrojs/starlight";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site:"https://nitkc-proken.github.io",
-	base: "/web-dev-intro",
-	integrations: [
-		starlight({
-			title: 'Web開発入門',     
-			 editLink: {
-        baseUrl: 'https://github.com/naotiki/web-dev-intro/edit/main/',
+  site: "https://nitkc-proken.github.io",
+  base: "/web-dev-intro",
+  integrations: [
+    starlight({
+      title: "Web開発入門",
+      editLink: {
+        baseUrl: "https://github.com/naotiki/web-dev-intro/edit/main/",
       },
-			tableOfContents: {
-				minHeadingLevel:1,
-				maxHeadingLevel: 3,
-			},
-			social: {
-				github: 'https://github.com/naotiki/web-dev-intro',
-			},
-			sidebar: [
-				{
-					label: 'Webの基礎',
-					autogenerate: { directory: 'basics' },
-				},
-				{
-					label: 'このサイトについて',
-					autogenerate: { directory: 'about' },
-				}
-			],
-		}),
-	],
+      tableOfContents: {
+        minHeadingLevel: 1,
+        maxHeadingLevel: 3,
+      },
+      social: {
+        github: "https://github.com/naotiki/web-dev-intro",
+      },
+      sidebar: [
+        {
+          label: "Webの基礎",
+          autogenerate: { directory: "basics" },
+        },
+        {
+          label: "このサイトについて",
+          autogenerate: { directory: "about" },
+        },
+      ],
+    }),
+  ],
 });
